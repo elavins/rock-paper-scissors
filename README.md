b446ac36f67196060264c02ed72251d3691f03ac
Emily <emlavins@gmail.com>
2022-03-02 18:15:56 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+Repository for doing The Odin Project's [Rock Paper Scissors](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors) assignment.
---
+++ script.js
@@ -0,0 +1,49 @@
+let results = [];
+
+for (let i = 0; i < 5; i++) {;
+    results.push(playGame());
+}
+
+printResults();
+
+
+
+function playGame() {
+    let move = playerMove();
+    while (move < 0) {
+        alert("That wasn't rock, paper or scissors! Try again.");
+        move = playerMove();
+    }
+    let cpu = cpuMove();
+
+    if (move == cpu) {
+        alert("Tie!");
+        return "tie";
+    } else if (move - 1 == cpu || move + 2 == cpu) {
+        alert("You win!");
+        return "player";
+    } else {
+        alert("Computer wins!");
+        return "cpu";
+    }
+}
+
+function cpuMove() {
+    return Math.round(Math.random() * 2 + 1);
+}
+
+function playerMove() {
+    let input = prompt("Rock, paper, or scissors?");
+    if (input.toLowerCase() == "rock") { return 1 }
+    else if (input.toLowerCase() == "paper") { return 2 }
+    else if (input.toLowerCase() == "scissors") { return 3 }
+    return -1;
+}
+
+function printResults() {
+    console.log("******** Final results! **********");
+    for (let i = 0; i < results.length; i++) { 
+        console.log(`Game${i+1}: ${results[i]}`);
+    }
+    console.log("*********************************");
+}
